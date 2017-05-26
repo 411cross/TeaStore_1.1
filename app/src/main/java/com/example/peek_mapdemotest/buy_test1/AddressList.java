f719a60b1226a888393f2892c76587bd96c7855b
嘉浚浚浚浚 <976207331@qq.com>
2017-05-27 00:39:11 +0800

    LongClick

--- app/src/main/java/com/example/peek_mapdemotest/buy_test1/AddressList.java
+++ app/src/main/java/com/example/peek_mapdemotest/buy_test1/AddressList.java
@@ -1,30 +1,33 @@
 package com.example.peek_mapdemotest.buy_test1;
 
 import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Build;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import listview.Address;
 import listview.AddressAdapter;
-import listview.Goods;
 import operation.UserOperation;
 
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class AddressList extends ActionBarActivity implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
+public class AddressList extends ActionBarActivity implements View.OnClickListener, PopupMenu.OnMenuItemClickListener, AdapterView.OnItemLongClickListener {
 
     private ArrayList<Address> addressList = new ArrayList<Address>();
 
@@ -56,6 +59,9 @@ public class AddressList extends ActionBarActivity implements View.OnClickListen
                 startActivity(intent);
             }
         });
+
+        addressListView.setOnItemLongClickListener(this);
+
     }
 
     @Override
@@ -65,6 +71,46 @@ public class AddressList extends ActionBarActivity implements View.OnClickListen
 
     @Override
     public boolean onMenuItemClick(MenuItem item) {
+        return false;
+    }
+
+
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        Address address =addressList.get(i);
+        final int addID = address.getAddress_id();
+        final View myLongClickView = layoutInflater.inflate(R.layout.useraddress_longclick, null);
+        Dialog alertDialog = new AlertDialog.Builder(this).
+                setView(myLongClickView).create();
+        alertDialog.show();
+        Button button1 = (Button) myLongClickView.findViewById(R.id.modifyButton);
+        Button button2 = (Button) myLongClickView.findViewById(R.id.deleteButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //修改对话框
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    ArrayList list = UserOperation.DeleteAddress(addID);
+                    if(Integer.parseInt((String)list.get(0))!=204){
+                        JSONObject object = new JSONObject((String)list.get(1));
+                        object.getString("message");
+                        Toast.makeText(getApplicationContext(),"",Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                Toast.makeText(getApplicationContext(),"",Toast.LENGTH_SHORT).show();
+            }
+        });
         return false;
     }
 }
