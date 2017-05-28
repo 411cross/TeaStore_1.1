045ad3894295a8e280c80073dd73c497e4c53e91
derrickJ <zhengbuxuan@gmail.com>
2017-05-28 10:31:43 +0800

    derrick - MOD Modify Message

--- app/src/main/java/com/example/peek_mapdemotest/buy_test1/AccountMessage.java
+++ app/src/main/java/com/example/peek_mapdemotest/buy_test1/AccountMessage.java
@@ -3,6 +3,7 @@ package com.example.peek_mapdemotest.buy_test1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import org.json.JSONException;
 
 import operation.GeneralOperation;
+import operation.GetImage;
 import operation.UserOperation;
 
 /**
@@ -29,13 +31,17 @@ public class AccountMessage extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_message);
 
+        avatarImage = (ImageView) findViewById(R.id.avatarImage);
+        accountName = (TextView) findViewById(R.id.account_name);
+
         try {
-            String avatarString = (String) UserOperation.userInfo(GeneralOperation.getUser()).get(2);
+            String avatarString = "http://" + (String) UserOperation.userInfo(GeneralOperation.getUser()).get(2);
+            Log.i("TEST GET2", (String) UserOperation.userInfo(GeneralOperation.getUser()).get(2));
+            new GetImage(avatarImage, avatarString).execute();
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        avatarImage = (ImageView) findViewById(R.id.avatarImage);
-        accountName = (TextView) findViewById(R.id.account_name);
+
         if(GeneralOperation.getUser().getName().equals("null")){
             accountName.setText(GeneralOperation.getUser().getUsername());
         }
